0a4dfff2137d579336118e3f24ca0b65ddfed28c
LoveKach <cryptonitik@gmail.com>
2023-01-30 21:12:51 +0300

    loop

--- loop.js
+++ loop.js
@@ -1,17 +1,28 @@
-let i = 1;
+// let i = 1;
 
-while (i < 19) {
-    console.log(i);
-    i = i + 1;
-}
-
-
+// while (i < 19) {
+//     console.log(i);
+//     i = i + 1;
+// }
 
 
 
- let k = 1;
 
- do {
-    console.log(k);
-    k = k + 1;
-} while (k < 19);
+
+//  let k = 2;
+
+//  do {
+//     console.log(k);
+//     k = k + 1;
+// } while (k < 19);
+
+
+// for (j = 3; j < 19; j++) {
+//     console.log(j);
+// }
+
+
+// for ( r = 5; r < 19;) {
+//     console.log(r);
+//     r = r + 4;
+// }
