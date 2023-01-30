5c527cf2ec5ef00c5be7338707b6c2dbf4ef953c
LoveKach <cryptonitik@gmail.com>
2023-01-30 21:47:58 +0300

    repeat

--- loop.js
+++ loop.js
@@ -27,32 +27,53 @@
 //     r = r + 4;
 // }
 
-let i = 1;
-while(i < 19) {
-    console.log(i);
-    i = i + 1;
+// let i = 1;
+// while(i < 19) {
+//     console.log(i);
+//     i = i + 1;
+// }
+
+// for(let l = 1; l < 19;l++ ) {
+//     console.log(l);
+// }
+
+// let k = 1;
+// for(; k < 19; k++) {
+//     console.log(k);
+// }
+
+// let j = 1;
+// for(; ; j++) {
+//     if (j < 19);
+//     console.log(j);
+    
+// }
+
+// while (true) {
+//     console.log('began');
+//     *break;*
+//     console.log('the end wich never happen')
+// }
+// console.log('the end');
+
+let g = 12;
+while (g >= 3) {
+    console.log(g);
+    g = g - 1;
 }
 
-for(let l = 1; l < 19;l++ ) {
-    console.log(l);
+for(let q = 1; q <= 4; q++) {
+    console.log(q);
 }
 
-let k = 1;
-for(; k < 19; k++) {
-    console.log(k);
+
+let w = 1;
+for(; w <= 3; w++) {
+    console.log(w);
 }
 
-let j = 1;
-for(; ; j++) {
-    if (j < 19);
-    console.log(j);
+for(t = 1; t <= 8 ; ) {
+    console.log(t);
+    t = t + 3;
     
 }
-
-while (true) {
-    console.log('began');
-    *break;*
-    console.log('the end wich never happen')
-}
-console.log('the end');
-
