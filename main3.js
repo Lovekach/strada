b930d15ba6bfefb8a6583d580451135fe923dfa0
LoveKach <cryptonitik@gmail.com>
2023-02-04 12:32:12 +0300

    code.mu

--- main3.js
+++ main3.js
@@ -21,9 +21,52 @@
 // console.log(obj['a']);
 
 
-let user = {
-    name: 'Valeriy',
-    surname: 'Zubov',
-    patronymic: 'Pavlovich'
-}
-console.log(user["surname"] + ' ' + user["name"] +  + user["patronymic"]);
+// let user = {
+//     name: 'Valeriy',
+//     surname: 'Zubov',
+//     patronymic: 'Pavlovich'
+// }
+// console.log(user["surname"] + ' ' + user["name"] +  + user["patronymic"]);
+
+
+// const date = {
+//     year: 2023,
+//     month: 1,
+//     day: 4
+// }
+// console.log(date['year'] + '-' + date['month'] + '-' + date['day']);
+// let obj = {
+// 	'1a': 1,
+// 	b2: 2,
+// 	'с-с': 3,
+// 	'd 4': 4,
+// 	e5: 5
+// };
+
+// console.log(obj['1a']);
+// console.log(obj.b2);
+// console.log(obj['c-c']);
+// console.log(obj['d 4']);
+// console.log(obj.e5);
+
+// let obj = {
+//     a: 1, 
+//     b: 2, 
+//     c: 3
+// };
+// obj['a'] = '!';
+// obj.b = '!';
+// console.log(obj);
+
+// let obj = {
+//     x: 1,
+//     y: 2, 
+//     z: 3
+// };
+
+// obj['x'] = 1**2; 
+// obj['y'] = 2**2; 
+// obj['z'] = 3**2; 
+
+// console.log(obj);
+
