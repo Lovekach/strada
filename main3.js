421f8d61499211812d16079cd71ea54424dd225c
LoveKach <cryptonitik@gmail.com>
2023-02-05 13:27:03 +0300

    todo

--- main3.js
+++ main3.js
@@ -1,15 +1,30 @@
 const list = {
-    "create a new practice task": "In Progress", 
-	"make a bed": "Done", 
-	"write a post": "To Do",
+    'create a new practice task': 'In Progress', 
+	'make a bed': 'Done', 
+	'write a post': 'To Do',
+}
+
+function changeStatus(taskName,status) {
+    list[taskName] = status;
+}
+function addTask(taskName) {
+    list[taskName] = 'To Do';
+}
+function deleteTask(taskName) {
+    delete list['make a bed'];
 }
 
 
+changeStatus("write a post", "Done"); 
+console.log(list['create a new practice task']);
+
+addTask('have a walk');
+console.log(list['have a walk']);
+
+deleteTask('make a bed'); 
+console.log(list['make a bed']);
 
 console.log(list);
-changeStatus("write a post", "Done") ;
-addTask('have a walk') ;
-deleteTask('have a walk') ;
-showList() ;
 
 
+// showList(); 
