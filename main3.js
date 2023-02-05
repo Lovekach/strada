4a05fc3454b82a25f5a6f887e3e7ef616d198d02
LoveKach <cryptonitik@gmail.com>
2023-02-05 20:55:06 +0300

    19

--- main3.js
+++ main3.js
@@ -1,7 +1,11 @@
 const list = {
     'create a new practice task': 'In Progress', 
-	'make a bed': 'Done', 
 	'write a post': 'To Do',
+    'wash a car' : 'In Progress',
+    'have a walk' : 'Done',
+    'make a bed' : 'In progress',
+    'have a breakfast' : 'To Do'
+
 }
 
 function changeStatus(taskName,status) {
@@ -11,9 +15,27 @@ function addTask(taskName) {
     list[taskName] = 'To Do';
 }
 function deleteTask(taskName) {
-    delete list['make a bed'];
+    delete list[taskName];
+}
+function showList() {
+    showWithCurrentStatus('To Do');
+    showWithCurrentStatus('In Progress');
+    showWithCurrentStatus('Done');
 }
 
+function showWithCurrentStatus(status) {
+    let flag = false;
+    for ( const p in list)  {
+        if (list[p] === status) {
+            flag = true;
+            console.log(p + ' : ' + list[p]);
+        }
+    }
+    if (!flag) {
+        console.log('Nothing is ' + status);
+    }
+    console.log();
+}
 
 changeStatus("write a post", "Done"); 
 console.log(list['create a new practice task']);
@@ -26,5 +48,9 @@ console.log(list['make a bed']);
 
 console.log(list);
 
+showList(); 
 
-// showList(); 
+// добавить новую задачу 
+// поменять данные задачи на 'In progress'
+//  удалить эту задачу из списка
+// вывести весь список чтобы убедиться чтобы этой задачи больше нет
