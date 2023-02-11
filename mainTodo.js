7d9365bf982c8ea4976bbcbfa2bafad6a33b0e25
LoveKach <cryptonitik@gmail.com>
2023-02-11 12:26:18 +0300

    todo-26

--- mainTodo.js
+++ mainTodo.js
@@ -1,28 +1,31 @@
 const list = [
     { name: 'Make a bed', status: 'In progress', priority: 'Low' },
-    { name: 'Create a new task', status: 'In Progress', priority: 'High' },
+    { name: 'Create a task', status: 'In Progress', priority: 'High' },
     { name: 'Write a post', status: 'To Do', priority: 'Low' },
     { name: 'Wash a car', status: 'In Progress', priority: 'High' },
     { name: 'Have a walk', status: 'Done', priority: 'High' },
 ];
 
+//Добавление объекта в массив
 function addTask(nameText, statusText, priorityText) {
     list.push({ name: nameText, status: statusText, priority: priorityText });
-    console.log(list);
+   
 
 }
+//Удаление объекта из массива
 function deleteTask(nameText, statusText, priorityText) {
     list.pop({ name: nameText, status: statusText, priority: priorityText });
-    console.log(list);
+   
 }
 
+//Изменение статуса у объекта, находящийся в массиве
 function changeStatus(nameText, statusText, priorityText) {
     list[4] = ({ name: nameText, status: statusText, priority: priorityText });
-    console.log(list);
-
+    
 }
 
-function compareNumeric(a, b) {
+//Сортировка объектов в массиве по статусу приоритета
+function rangeStatus(a, b) {
     if (a.priority === b.priority) {
         return 0;
     }
@@ -36,13 +39,24 @@ function compareNumeric(a, b) {
 
 }
 
-list.sort(compareNumeric);
-console.log(list);
+
+
 
 
 addTask('Go to the cinema', 'In progress', 'Low');
+console.log(list);
+
 deleteTask('Write a post', 'To Do', 'Low');
+console.log(list);
+
 changeStatus('Have a walk', 'To Do', 'Low');
+console.log(list);
+
+
+
+list.sort(rangeStatus);
+console.log(list);
+
 
 
 
