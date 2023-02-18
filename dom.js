524dc40999c2994dfd88aaf5e672066f166e339a
LoveKach <cryptonitik@gmail.com>
2023-02-18 17:23:50 +0300

    dom

---
+++ dom.js
@@ -0,0 +1,8 @@
+const number1 = document.body;
+console.log(body.firstElementChild);
+
+const number2 = document.body;
+console.log(body.firsElementtChild.nextSibling);
+
+const number3 = document.body;
+console.log(body.lastElementChild.lastElementChild);
