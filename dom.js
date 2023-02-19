3f3356b24ca605704f18fe90891e067021d1c29c
LoveKach <cryptonitik@gmail.com>
2023-02-19 22:04:00 +0300

    dom

--- dom.js
+++ dom.js
@@ -3,6 +3,3 @@ console.log(body.firstElementChild);
 
 const number2 = document.body;
 console.log(body.firsElementtChild.nextSibling);
-
-const number3 = document.body;
-console.log(body.lastElementChild.lastElementChild);
