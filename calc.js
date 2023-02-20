9ba3a98141bee21d5565d136c9dc14f9ef305dd1
LoveKach <cryptonitik@gmail.com>
2023-02-20 11:55:28 +0300

    calc

---
+++ calc.js
@@ -0,0 +1,5 @@
+const inputANumber = document.querySelector('.input_number_a');
+const selectOperation = document.querySelector('.select_operation');
+const inputBNumber = document.querySelector('.input_number_b');
+const buttonEquals = document.querySelector('.equals');
+const outputResult = document.querySelector('.output_result');
--- dom.js
+++
@@ -1,5 +0,0 @@
-const number1 = document.body;
-console.log(body.firstElementChild);
-
-const number2 = document.body;
-console.log(body.firsElementtChild.nextSibling);
